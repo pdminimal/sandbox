4e75d99ccf49aeb7f3123c859fa75f4b82137f48
pdminimal <pdminimal@gmail.com>
2019-02-21 02:05:30 +0900

    Add text sgmentation

---
+++ text_segmentation.py
@@ -0,0 +1,53 @@
+import jaconv
+import random
+import re
+from collections import Counter
+
+with open('corpus.txt') as f:
+    text = f.read()
+
+orig = re.sub(r'(［.+?］|《.+?》)', '', jaconv.h2z(text, digit=True, ascii=True),
+                   flags=(re.MULTILINE | re.DOTALL))
+import random
+def make_next_segs(orig, one_grams, thres=1000):
+    one_gram_counts = Counter(one_grams).most_common(thres)
+
+    two_grams = [''.join(one_grams[i: i + 2]) for i in range(0, len(one_grams), 2)]
+    two_grams2 = [''.join(one_grams[i: i + 2]) for i in range(1, len(one_grams), 2)]
+    two_gram_counts = Counter(two_grams + two_grams2).most_common(thres)
+    # common_two_grams = [i[0] for i in two_gram_counts]
+    # c_thres = max(c_thres, two_gram_counts[-1][1])
+    segs = two_gram_counts + one_gram_counts
+    segs.sort(key=lambda a: -a[1])
+    segs = [i for i in segs[:thres] if i[1] > int(len(one_grams) / thres / 5 + random.random())]
+    segs.sort(key=lambda a: -len(a[0]))
+    pattern = f"({'|'.join([i[0] for i in segs])}|.)"
+    return [i for i in re.findall(pattern, orig) if i]
+
+segs = orig
+prev = segs
+for i in range(300):
+    newsegs = []
+    for seg in segs:
+        if random.randint(0, 5) > 0:
+            newsegs.append(seg)
+        else:
+            newsegs.extend(seg)
+    l = len(set(segs)) * 2 // 2 - 1
+    print(l)
+    segs = make_next_segs(orig, segs, l)
+    if len(set(prev)) >= len(set(segs)):
+        print(i, '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        break
+    print(i, len(prev), len(segs))
+    prev = segs
+c = Counter(segs).most_common(len(segs))
+
+print(orig[2000:2300])
+print([jaconv.z2h(i, digit=True, ascii=True) for i in segs[2000:2300]])
+c.sort(key=lambda a: (-len(a[0]), -a[1]))
+pattern = f"({'|'.join([i[0] for i in c[:l]])}|)"
+segs = [i for i in re.findall(pattern, orig) if i]
+print([jaconv.z2h(i, digit=True, ascii=True) for i in segs[2000:2300]])
+#print(c)
+print(i)
