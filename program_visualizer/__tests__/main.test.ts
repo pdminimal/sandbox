edc7a165364b848ce3230157c43a5d01ffdfabc5
pdminimal <pdminimal@gmail.com>
2019-04-07 20:30:15 +0900

    Create Rule class

--- program_visualizer/__tests__/main.test.ts
+++
@@ -1,6 +0,0 @@
-import {Parser} from '../main';
-
-test('initialize parser', () => {
-  const parser = new Parser('test');
-  expect(parser.src).toBe('test');
-});
--- program_visualizer/main.ts
+++ program_visualizer/main.ts
@@ -1,19 +1,6 @@
-export class Parser {
-  src: string;
-  cursor: number;
-  stack: string[];
+// import {Interpreter} from '../interpreter';
 
-  constructor(src: string) {
-    this.src = src;
-    this.cursor = 0;
-    this.stack = [];
-  }
-
-  step() {}
-}
-
-const binarySearch = `
-def binary_search(a, val):
+export const binarySearch = `def binary_search(a, val):
     l = 0
     r = len(a) - 1
 
@@ -29,12 +16,9 @@ def binary_search(a, val):
 
     return -1
 
-binary_search([-5, 1, 3, 4, 5, 7, 18, 19], 7)
-`;
+binary_search([-5, 1, 3, 4, 5, 7, 18, 19], 7)`;
 
-export const main = () => {
-  const src = document.getElementById('src');
-  if (src) {
-    src.textContent = binarySearch;
-  }
-};
+const src = document.getElementById('src');
+if (src) {
+  src.textContent = binarySearch;
+}
