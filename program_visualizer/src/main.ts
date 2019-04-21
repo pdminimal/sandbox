6c4615dc8140883be4d714aa80cc6b958eef9182
pdminimal <pdminimal@gmail.com>
2019-04-21 12:40:03 +0900

    Highlights strings in animation

--- program_visualizer/src/main.ts
+++ program_visualizer/src/main.ts
@@ -18,7 +18,7 @@ let binarySearch = `def binary_search(a, val):
 binary_search([-5, 1, 3, 4, 5, 7, 18, 19], 7)`;
 
 const replaces = [
-  [[
+  [
     '[-5, 1, 3, 4, 5, 7, 18, 19], 7)', `a=[-5, 1, 3, 4, 5, 7, 18, 19], val=7):
     l = 0
     r = len(a) - 1
@@ -34,14 +34,13 @@ const replaces = [
 
     return -1
 `
-  ]],
-  [[]],
-  [['len(a)', '8']],
-  [['8 - 1', '7']],
-  [['l <=', '0 <=']],
-  [['<= r', '<= 7']],
-  [['0 <= 7', 'true']],
-  [[
+  ],
+  ['len(a)', '8', 'a=[-5, 1, 3, 4, 5, 7, 18, 19]'],
+  ['8 - 1', '7'],
+  ['l <=', '{{0}} <=', 'l = 0'],
+  ['<= r', '<= {{7}}', 'r = 7'],
+  ['0 <= 7', 'true'],
+  [
     `
     while true:
         mid = (l + r) // 2
@@ -61,16 +60,17 @@ const replaces = [
     else:
         l = mid + 1
 `
-  ]],
+  ],
 
-  [['l + r', '0 + 7']],
-  [['(0 + 7)', '7']],
-  [['7 // 2', '3']],
-  [['a[mid] ==', 'a[3] ==']],
-  [['a[3]', '4']],
-  [['== val', '== 7']],
-  [['4 == 7', 'false']],
-  [[
+  ['l + r', '{{0}} + r', 'l = 0'],
+  ['0 + r', '0 + {{7}}', 'r = 7'],
+  ['(0 + 7)', '7'],
+  ['7 // 2', '3'],
+  ['a[mid] ==', 'a[{{3}}] ==', 'mid = 3'],
+  ['a[3]', '4', ' 4'],
+  ['== val', '== {{7}}', 'val=7'],
+  ['4 == 7', 'false'],
+  [
     `
     if false:
         return mid
@@ -85,12 +85,12 @@ const replaces = [
     else:
         l = mid + 1
 `
-  ]],
-  [['val <', '7 <']],
-  [['a[mid]', 'a[3]']],
-  [['a[3]', '4']],
-  [['7 < 4', 'false']],
-  [[
+  ],
+  ['val <', '{{7}} <', 'val=7'],
+  ['a[mid]', 'a[{{3}}]', 'mid = 3'],
+  ['a[3]', '4', ' 4'],
+  ['7 < 4', 'false'],
+  [
     `
     elif false:
         r = mid - 1
@@ -100,11 +100,11 @@ const replaces = [
     `
     l = mid + 1
 `
-  ]],
-  [['mid + 1', '3 + 1']],
-  [['3 + 1', '4']],
-  [['    l = 0\n', '']],
-  [[
+  ],
+  ['mid + 1', '{{3}} + 1', 'mid = 3'],
+  ['3 + 1', '{{4}}'],
+  ['    l = 0\n', ''],
+  [
     `
     mid = 3
     l = 4
@@ -121,11 +121,11 @@ const replaces = [
         else:
             l = mid + 1
 `
-  ]],
-  [['l <=', '4 <=']],
-  [['<= r', '<= 7']],
-  [['4 <= 7', 'true']],
-  [[
+  ],
+  ['l <=', '{{4}} <=', 'l = 4'],
+  ['<= r', '<= {{7}}', 'r = 7'],
+  ['4 <= 7', 'true'],
+  [
     `
     while true:
         mid = (l + r) // 2
@@ -145,17 +145,18 @@ const replaces = [
     else:
         l = mid + 1
 `
-  ]],
+  ],
 
-  [['l + r', '4 + 7']],
-  [['(4 + 7)', '11']],
-  [['11 // 2', '5']],
-  [['    mid = 3\n', '']],
-  [['a[mid] ==', 'a[5] ==']],
-  [['a[5]', '7']],
-  [['== val', '== 7']],
-  [['7 == 7', 'true']],
-  [[
+  ['l + r', '{{4}} + r', 'l = 4'],
+  ['4 + r', '4 + {{7}}', 'r = 7'],
+  ['(4 + 7)', '11'],
+  ['11 // 2', '5'],
+  ['    mid = 3\n', ''],
+  ['a[mid] ==', 'a[{{5}}] ==', 'mid = 5'],
+  ['a[5]', '7', ' 7'],
+  ['== val', '== {{7}}', 'val=7'],
+  ['7 == 7', 'true'],
+  [
     `
     if true:
         return mid
@@ -167,9 +168,9 @@ const replaces = [
     `
     return mid
 `
-  ]],
-  [['return mid', 'return 5']],
-  [[]],
+  ],
+  ['return mid', 'return {{5}}', 'mid = 5'],
+  [],
 ];
 
 const src = document.getElementById('src');
@@ -182,12 +183,51 @@ if (src) {
     if (i === 0) {
       binarySearch = binarySearch.slice(256);
     } else if (i === replaces.length - 1) {
-      binarySearch = '5';
+      binarySearch = '{{5}}';
     }
-    for (let j = 0; j < curReplace.length; j++) {
-      binarySearch = binarySearch.replace(curReplace[j][0], curReplace[j][1]);
+    if (curReplace.length) {
+      let replace = curReplace[1];
+      if (replace.indexOf('{{') < 0) {
+        replace = '{{' + replace + '}}';
+      }
+      binarySearch = binarySearch.replace(curReplace[0], replace);
+      if (curReplace.length > 2) {
+        binarySearch =
+            binarySearch.replace(curReplace[2], '{%' + curReplace[2] + '%}');
+      }
     }
-    src.textContent = prefix + binarySearch;
+    const spans: HTMLSpanElement[] = [];
+    const srcText = prefix + binarySearch;
+    let index = srcText.search(/\{\{|\{[%]/);
+    let start = 0;
+    while (index >= start) {
+      const startStr = srcText.slice(index, index + 2);
+      const endStr = startStr === '{{' ? '}}' : '%}';
+      let span = document.createElement('span');
+      span.textContent = srcText.slice(start, index);
+      spans.push(span);
+      const end = srcText.indexOf(endStr, index + 2);
+      span = document.createElement('span');
+      span.textContent = srcText.slice(index + 2, end);
+      span.classList.add(startStr === '{{' ? 'emphasized' : 'def');
+      spans.push(span);
+      start = end + 2;
+      index = srcText.slice(start).search(/\{\{|\{[%]/) + start;
+      if (index > srcText.length || index < 3) {
+        break;
+      }
+    }
+    const span = document.createElement('span');
+    span.textContent = srcText.slice(start);
+    spans.push(span);
+    // src.textContent = prefix + binarySearch;
+    while (src.firstChild) {
+      src.firstChild.remove();
+    }
+    spans.forEach(element => {
+      src.appendChild(element);
+    });
+    binarySearch = binarySearch.replace(/\{\{|\}\}|\{\%|\%\}/g, '');
     i += 1;
     if (i < replaces.length) {
       setTimeout(step, 1000);
