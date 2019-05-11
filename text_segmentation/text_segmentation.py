9a86d1e2d97a2a1735c93b333bdbb36800d4802b
pdminimal <pdminimal@gmail.com>
2019-05-11 11:11:41 +0900

    Refactor

--- text_segmentation/text_segmentation.py
+++ text_segmentation/text_segmentation.py
@@ -8,13 +8,10 @@ import unicodedata
 with open('corpus.txt') as f:
     text = f.read()
 
-text = re.sub(r'(［.+?］|《.+?》)', '', text, flags=(re.MULTILINE | re.DOTALL))
-text = re.sub('\u3000+', '\u3000', text, flags=(re.MULTILINE | re.DOTALL))
-text = re.sub('\n', '', text, flags=(re.MULTILINE | re.DOTALL))
 orig = jaconv.h2z(text, digit=True, ascii=True)
 
 
-def make_next_segs(orig, one_grams):
+def step(orig, one_grams):
     volume = len(set(one_grams))
 
     one_gram_counts = Counter(one_grams).most_common(volume)
@@ -25,10 +22,10 @@ def make_next_segs(orig, one_grams):
     two_gram_counts = Counter(two_grams + two_grams2).most_common(volume // 5)
     vocab = two_gram_counts + one_gram_counts
     vocab.sort(key=lambda a: (-len(a[0]), -a[1]))
-    pattern = f"({'|'.join([i[0] for i in vocab])}|.)"
+    pattern = f"({'|'.join(i[0] for i in vocab)}|.)"
     vocab = Counter(re.findall(pattern, orig)).most_common(volume)
     vocab.sort(key=lambda a: -len(a[0]))
-    pattern = f"({'|'.join([i[0] for i in vocab])}|.)"
+    pattern = f"({'|'.join(i[0] for i in vocab)}|.)"
     return re.findall(pattern, orig)
 
 
@@ -36,11 +33,10 @@ print("{:,}".format(len(orig)))
 segs = orig[: 10 * 1000 * 1000]
 TARGET_VOCAB = 10000
 for i in range(100):
-    segs = make_next_segs(orig, segs)
-    num = len(set(segs))
-    score = num * len(segs)
-    print(score, num)
-    if num > TARGET_VOCAB:
+    segs = step(orig, segs)
+    num_vocab = len(set(segs))
+    print(num_vocab)
+    if num_vocab > TARGET_VOCAB:
         break
 
 start = 30000
